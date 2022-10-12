0b3f62c569520b65913375b44c8e3886fb3f0f3b
marahim34 <marahim34@gmail.com>
2022-10-12 13:24:21 +0300

    toast added

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
+
 const Options = ({ option }) => {
+    // const {options}
+
+    // const handlerError = () => {
+    //     let message;
+    //     if (option === correctAnswer) {
+    //         return 'Your answer is Correct'
+    //     }
+    //     else {
+    //         return 'Your answer is wrong. Try Again!'
+    //     }
+    // }
     return (
         <div className='flex inline-block pt-2'>
             <label><input type="radio" name="radio-2" className="radio radio-primary mr-3" /> {option}</label>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,26 +2,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import Options from '../Options/Options';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const QuizDetails = ({ questionAll }) => {
     const { question, options, correctAnswer } = questionAll;
     console.log(questionAll);
+    const answer = correctAnswer;
+    const notify = () => toast(answer);
 
     // con
 
-    const addHandler = () => {
-        const answer = correctAnswer;
-        console.log(answer);
-        return correctAnswer;
-    }
+    // const addHandler = () => {
+    //     const answer = correctAnswer;
+    //     console.log(answer);
+    //     return correctAnswer;
+    // }
 
     return (
         <div>
             <div className='flex items-center'>
                 <h3 className='pt-3 font-semibold pr-10'>Question: {question.slice(3, -4)}</h3>
-                <button><FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
-                </button>
-                <p className='pl-12'>{correctAnswer}</p>
+                <button onClick={notify}><FontAwesomeIcon icon={faEye}> </FontAwesomeIcon></button><ToastContainer />
+                {/* <p className='pl-12'>{correctAnswer}</p> */}
             </div>
             <div>
                 {
